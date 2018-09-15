174720fc0d0e465485d776c86dfeee6e60d8ad04
Ian321 <ignaz.k@live.de>
2018-09-16 00:26:37 +0200

    fix: some paths have spaces

--- postinstall.js
+++ postinstall.js
@@ -76,11 +76,18 @@ let postInstall = {
     let url = 'https://github.com/vuejs/vue-devtools.git';
     let branch = '-b master';
 
+    let clonedLocation;
+    if (process.platform === 'win32') {
+      clonedLocation = `"${this.data.clonedLocation}"`;
+    } else {
+      clonedLocation = this.data.clonedLocation.replace(/[ ]/g, '\\ ');
+    }
+
     let args = [
       executable,
       url,
       branch,
-      this.data.clonedLocation
+      clonedLocation
     ].join(' ').trim();
 
     try {
@@ -93,7 +100,7 @@ let postInstall = {
         executable,
         url,
         branch,
-        this.data.clonedLocation
+        clonedLocation
       ].join(' ').trim();
 
       try {
